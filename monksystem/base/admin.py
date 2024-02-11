36337fb980e935dd2cd96cd2cc28d533e2862139
Aliaan <aliaan.azam02@gmail.com>
2024-02-11 16:48:37 +0100

    Connected application to database and created models for doctor, patient, appointment and vitals.

--- monksystem/base/admin.py
+++ monksystem/base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Doctor, Patient, Appointment, Vitals
+
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Appointment)
+admin.site.register(Vitals)
--- monksystem/base/models.py
+++ monksystem/base/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=50)
+    mobile = models.IntegerField()
+    specialization = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
+class Patient(models.Model):
+    name = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10)
+    address = models.CharField(max_length=100)
+    mobile = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE) # When doctor is deleted, appointment is also deleted.
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE) # When patient is deleted, appointment is also deleted.
+    description = models.TextField(null = True, blank = True) # Description of appointment. Makes sure the values can be left blank. 
+    date = models.DateField() # Date of appointment. 
+    time = models.TimeField() # Time of appointment.
+    updated = models.DateTimeField(auto_now = True) # Takes a snapshot of anytime the table (model instance) is updated. Takes a timestamp every time appointment is updated.
+    created = models.DateTimeField(auto_now_add = True) # Takes a timestamp of when the instance was created.
+    
+    def __str__(self):
+        return self.doctor.name+"--"+self.patient.name
+    
+    
+class Vitals(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE) # When patient is deleted, appointment is also deleted.
+    description = models.TextField(null = True, blank = True) # Description of appointment. Makes sure the values can be left blank. 
+    #heartRate = models.IntegerField(null=True)
+    #oxygen = models.IntegerField(null=True)
+    #bodyTemperature = models.IntegerField(null=True)
+    #respirationRate = models.IntegerField(null=True)
+    #bloodPressure = models.IntegerField(null=True)
+    updated = models.DateTimeField(auto_now = True) # Takes a snapshot of anytime the table (model instance) is updated. Takes a timestamp every time appointment is updated.
+    created = models.DateTimeField(auto_now_add = True) # Takes a timestamp of when the instance was created.
+    
+    def __str__(self):
+        return self.patient.name
